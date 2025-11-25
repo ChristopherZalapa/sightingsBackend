7bddde8ee80dbce056541cd02b125453d1c52b18
Christopher Zalapa <63930010+ChristopherZalapa@users.noreply.github.com>
2025-11-24 16:12:33 -0800

    Added routes

--- handlers/routeHandlers.js
+++ handlers/routeHandlers.js
@@ -1,8 +1,14 @@
 import { getData } from "../Utils/getData.js";
 import { sendResponse } from "../Utils/sendResponse.js";
+import { parseJSONBody } from "../Utils/parseJSONBody.js";
 
 export async function handleGet(res) {
 	const data = await getData();
 	const content = JSON.stringify(data);
 	sendResponse(res, 200, "application/json", content);
+}
+
+export async function handlePost(req, res) {
+	const rawBody = await parseJSONBody(req);
+	console.log(rawBody);
 }
